3acf59b2dde47c2750c1c706dd7e23a49202fb4d
ajith <ajithraveendranr@gmail.com>
2022-07-29 16:37:07 +0530

    api call complete

--- weather-appReact/src/App.js
+++ weather-appReact/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Weather from './Components/Weather';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [city,setCity] = useState('London');
+
+
+  let latitude;
+       let longitude;
+
+       useEffect(() => {
+
+         getWeatherData();
+        },[])
+
+function getWeatherData () {
+      //https://geocoding-api.open-meteo.com/v1/search?name=Berlin
+
+  axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`).then(res => {
+      
+      //console.log(res.data);
+       latitude = res.data.results[0].latitude;
+       longitude = res.data.results[0].longitude;
+
+       wdata(latitude,longitude)
+ 
+      })
+
+}
+
+let data;
+
+const wdata = async(latitude,longitude) => {
+let res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain,cloudcover&daily=temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum&current_weather=true&timezone=Asia%2FSingapore`);
+data = await res.json();
+//console.log(data.daily[0]);
+console.log(data);
+//console.log(data.current_weather);
+
+//props.data(data);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
     </div>
   );
 }
---
+++ weather-appReact/src/Components/Weather.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+ let apiData;
+function Weather(props) {
+    
+    return (
+        <div className='A'>
+           
+        </div>
+    );
+}
+export default Weather;
+export { apiData};
